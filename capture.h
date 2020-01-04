31e005e1d2efdffb3af6377079023d95f626e5b2
Michael Henry <froderick@gmail.com>
2020-01-04 14:57:49 -0500

    nuked a bunch of unused stuff

--- capture.h
+++ capture.h
@@ -1,17 +1,15 @@
 #include <stdint.h>
 
+#define CAPTURE_MAX_FACES 10
+#define CAPTURE_CIRCLE_RADIUS 30
+#define CAPTURE_WIDTH 320
+#define CAPTURE_HEIGHT 240
+
 typedef struct Capture* Capture_t;
 
 typedef struct CaptureFace {
   int x, y, width, height;
 } CaptureFace;
-
-#define CAPTURE_MAX_FACES 10
-
-#define CAPTURE_CIRCLE_RADIUS 30
-
-#define CAPTURE_WIDTH 320
-#define CAPTURE_HEIGHT 240
 
 typedef struct CaptureResults {
   uint16_t numFaces;
--- cnc/test/thunder_cnc/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns thunder-cnc.core-test
-  (:require [clojure.test :refer :all]
-            [thunder-cnc.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
